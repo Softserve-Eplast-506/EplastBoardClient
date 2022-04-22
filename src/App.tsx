4c2a4c6a720d7dd25aa9e52e5d3b32ac80ecbdbf
Tomash-Klaus <tomashivskymykola@gmail.com>
2022-04-22 11:40:42 +0300

    Architecture added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import { Layout } from 'antd';
-import { Header } from 'antd/lib/layout/layout';
+import { Content, Header } from 'antd/lib/layout/layout';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css';
-import BasicLayout from './components/BasicLayout';
+import LeftSideBar from './components/LeftSideBar/LeftSideBar';
 
 function App() {
   return (
     <>
-      <BasicLayout/>
+
+
+      <Layout style={{ minHeight: "100vh" }}>
+      <LeftSideBar />
+
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              Bill is a cat.
+            </div>
+          </Content>
+
+        </Layout>
+      </Layout>
+
+
     </>
   );
 }
