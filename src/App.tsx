181d13deb32544ae6529ae4301eb3fce6ebb8a17
Omeliukh-Nazar <91065958+Omeliukh-Nazar@users.noreply.github.com>
2022-04-27 14:01:34 +0300

    Revert "Added new state for current board"
    This reverts commit cc6b6018ca14f065f37ea7d099e50bc8f0efd2bf.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { Layout } from "antd";
-import { useEffect } from "react";
-import "./App.css";
-import ContentSpace from "./components/ContentSpace/ContentSpace";
-import CreateCardModal from "./components/ContentSpace/CreateCardModal";
-import HeaderBar from "./components/HeaderBar/HeaderBar";
-import LeftSideBar from "./components/LeftSideBar/LeftSideBar";
-import { useTable } from "./store/store";
+import { Layout } from 'antd';
+import { useEffect } from 'react';
+import './App.css';
+import ContentSpace from './components/ContentSpace/ContentSpace';
+import CreateCardModal from './components/ContentSpace/CreateCardModal';
+import HeaderBar from './components/HeaderBar/HeaderBar';
+import LeftSideBar from './components/LeftSideBar/LeftSideBar';
+import { useTable } from './store/store';
 
 function App() {
   const [, actions] = useTable();
-  useEffect(() => {
+  useEffect(() =>{
     actions.getBoards();
-  }, []);
+  },[])
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <HeaderBar />
-      <Layout className="layoutStyles">
-        <LeftSideBar />
-        <CreateCardModal />
-        <ContentSpace />
+      <Layout style={{ minHeight: "100vh" }}>
+        <HeaderBar />
+        <Layout className='layoutStyles'>
+          <LeftSideBar />
+          <CreateCardModal/>
+          <ContentSpace />
+        </Layout>
       </Layout>
-    </Layout>
   );
 }
+
 
 export default App;
--- src/components/LeftSideBar/LeftSideBar.tsx
+++ src/components/LeftSideBar/LeftSideBar.tsx
@@ -1,13 +1,8 @@
-import { useTable } from "../../store/store";
-import { Button, Layout, Menu } from "antd";
-import "./LeftSideBar.css";
+import { useTable } from '../../store/store';
+import { Layout, Menu } from "antd";
+import './LeftSideBar.css';
 import "antd/dist/antd.min.css";
-import {
-  DeleteOutlined,
-  DoubleRightOutlined,
-  EditOutlined,
-  LeftOutlined,
-} from "@ant-design/icons";
+import { DoubleRightOutlined, LeftOutlined } from '@ant-design/icons';
 
 const { Sider } = Layout;
 
@@ -19,7 +14,7 @@ const LeftSideBar = () => {
   };
 
   const collapseSideBarOnBreakpoint = async () => {
-    if (!state.isSideBarHidden) {
+    if (!state.isSideBarHidden){
       collapseSideBar();
     }
   };
@@ -28,44 +23,15 @@ const LeftSideBar = () => {
       <div className="sidebar">
         <div className="collapsedButton trigger" onClick={collapseSideBar}>
           {state.isSideBarHidden ? (
-            <div>
-              <DoubleRightOutlined />
-            </div>
+            <div><DoubleRightOutlined/></div>
           ) : (
-            <div className="collapsedButtonHide white-text">
-              List of Boards
-              <LeftOutlined />
-            </div>
+            <div className="collapsedButtonHide white-text">List of Boards<LeftOutlined/></div>
           )}
         </div>
 
-        <Sider
-          className="sidebar"
-          collapsedWidth={50}
-          breakpoint="sm"
-          onBreakpoint={collapseSideBarOnBreakpoint}
-          trigger={null}
-          collapsible
-          collapsed={state.isSideBarHidden}
-        >
+        <Sider className="sidebar" collapsedWidth={50} breakpoint='sm' onBreakpoint={collapseSideBarOnBreakpoint} trigger={null} collapsible collapsed={state.isSideBarHidden} >
           <div className="logo" />
-          <Menu
-            theme="dark"
-            className="menu"
-            mode="inline"
-            items={state.menuItems}
-            onSelect={(event) => actions.setCurrentBoard(Number(event.key))}
-          />
-          {!state.isSideBarHidden ? (
-            <div className="buttonsBlock">
-              <Button className="itemButton">
-                <EditOutlined />
-              </Button>
-              <Button className="itemButton">
-                <DeleteOutlined />
-              </Button>
-            </div>
-          ) : null}
+          <Menu theme="dark" className='menu' defaultSelectedKeys={['1']} mode="inline" items={state.menuItems} />
         </Sider>
       </div>
     </>
