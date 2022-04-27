5833c74cf1532e3119e4b932cce0d7e7f7a57eb9
Omeliukh-Nazar <91065958+Omeliukh-Nazar@users.noreply.github.com>
2022-04-27 14:01:26 +0300

    Added new state for current board

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,114 +1,99 @@
-import { MenuProps } from "antd";
-import { createStore, createHook, Action } from "react-sweet-state";
-import { getAllBoards } from "../api/boardsApi";
-import { AddCard, getAllCards } from "../api/cardsApi";
-import Board from "../models/Board";
-import CardM from "../models/Card";
+import { MenuProps } from 'antd';
+import { createStore, createHook, Action } from 'react-sweet-state';
+import { getAllBoards } from '../api/boardsApi';
+import { AddCard, getAllCards } from '../api/cardsApi';
+import Board from '../models/Board';
+import CardM from '../models/Card';
 
-type MenuItem = Required<MenuProps>["items"][number];
+type MenuItem = Required<MenuProps>['items'][number];
 
 type State = {
-  isSideBarHidden: boolean;
-  isEditCardModalHidden: boolean;
-  menuItems: MenuItem[];
-  cards: CardM[];
-  boards: Board[];
-  currentBoard?: Board;
+    isSideBarHidden: boolean,
+    isEditCardModalHidden: boolean,
+    menuItems: MenuItem[],
+    cards: CardM[],
 };
 
 const initialState: State = {
-  isSideBarHidden: false,
-  isEditCardModalHidden: false,
-  menuItems: [],
-  cards: [],
-  boards: [],
-  currentBoard: undefined,
+    isSideBarHidden: false,
+    isEditCardModalHidden: false,
+    menuItems: [],
+    cards: [],
 };
 
 const actions = {
-  getBoards:
-    (): Action<State> =>
-    async ({ setState, getState }) => {
-      const boards: Board[] = (await getAllBoards()).data;
-      let items: MenuItem[] = [];
-      boards.map((board: Board) => {
-        items.push(getItem(board.title, board.id));
-      });
-      setState({
-        boards: boards,
-        menuItems: items,
-      });
-    },
+      
+    getBoards:
+        (): Action<State> => 
+            async ({ setState, getState }) => {
+                const boards: Board[] = (await getAllBoards()).data;
+                let items: MenuItem[] = [];
+                boards.map((board: Board) => {
+                    items.push(getItem(board.title, board.id));
+                })
+                setState({
+                    menuItems: items
+                });
 
-  setCurrentBoard:
-    (boardId: number): Action<State> =>
-    async ({ setState, getState }) => {
-      const board = getState().boards.find(
-        (board: Board) => board.id === boardId
-      );
-      console.log(board);
-      setState({
-        currentBoard: board,
-      });
-    },
+            },
 
-  hideSideBar:
+    hideSideBar:
+        (): Action<State> =>
+            ({ setState, getState }) => {
+                setState({
+                    isSideBarHidden: !getState().isSideBarHidden
+                });
+            },
+    hideEditCardModal:
     (): Action<State> =>
-    ({ setState, getState }) => {
-      setState({
-        isSideBarHidden: !getState().isSideBarHidden,
-      });
-    },
-  hideEditCardModal:
+        ({ setState, getState }) => {
+            setState({
+                isEditCardModalHidden: !getState().isEditCardModalHidden
+            });
+        },
+    getAllCards:
     (): Action<State> =>
-    ({ setState, getState }) => {
-      setState({
-        isEditCardModalHidden: !getState().isEditCardModalHidden,
-      });
-    },
-  getAllCards:
-    (): Action<State> =>
-    async ({ setState, getState }) => {
-      setState({
-        cards: await getCards(),
-      });
-    },
-  createCard:
+        async ({ setState, getState }) => {
+            setState({
+                cards: await getCards()
+            });
+        },
+    createCard:
     (Card: CardM): Action<State> =>
-    async ({ setState, getState }) => {
-      await createCard(Card);
-      setState({
-        cards: await getCards(),
-      });
-    },
+        async ({ setState, getState }) => {
+            await createCard(Card);
+            setState({
+                cards: await getCards()
+            });
+        },
 };
 
 const Store = createStore({
-  initialState,
-  actions,
+    initialState,
+    actions,
 });
 
 function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode
-): MenuItem {
-  return {
-    key,
-    icon,
-    label,
-  } as MenuItem;
-}
-
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+  ): MenuItem {
+    return {
+      key,
+      icon,
+      label,
+    } as MenuItem;
+  }
+        
 const getCards = async () => {
-  const response = await getAllCards();
-  return response.data;
+    const response = await getAllCards();
+    return response.data;
 };
 
 const createCard = async (Card: CardM) => {
-  console.log("crac");
-  const response = await AddCard(Card);
-  console.log(response.data);
-  return response.data;
+    console.log("crac");
+    const response = await AddCard(Card);
+    console.log(response.data);
+    return response.data;
 };
 export const useTable = createHook(Store);
