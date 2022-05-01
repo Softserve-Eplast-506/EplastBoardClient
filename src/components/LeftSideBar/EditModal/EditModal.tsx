a591c515a7e6b9c76270a9faa1958c4e62f8c40b
Omeliukh-Nazar <91065958+Omeliukh-Nazar@users.noreply.github.com>
2022-05-02 00:55:20 +0300

    Board Title is editeble

--- src/components/LeftSideBar/EditModal/EditModal.tsx
+++ src/components/LeftSideBar/EditModal/EditModal.tsx
@@ -1,23 +1,16 @@
-import { Modal, Button, Input } from 'antd';
-import React from 'react';
-import { editBoardNameddBoard } from '../../../api/boardsApi';
-import Board from '../../../models/Board';
-import { useTable } from '../../../store/store';
-
+import { Modal, Input } from "antd";
+import { editBoardNameBoard } from "../../../api/boardsApi";
+import { useTable } from "../../../store/store";
 
 const EditModal = () => {
   const [state, actions] = useTable();
 
-  const showModal = () => {
-    actions.showEditBoardModal();
-  };
-
   const handleOk = async () => {
-    let renamedBoard=state.currentBoard;
-    renamedBoard.title=state.editBoardName;
+    let renamedBoard = state.currentBoard;
+    renamedBoard.title = state.editBoardName;
     actions.showEditBoardModal();
-    await editBoardNameddBoard(renamedBoard);
-    actions.getBoards(); 
+    await editBoardNameBoard(renamedBoard);
+    actions.getBoards();
   };
 
   const handleCancel = () => {
@@ -25,20 +18,20 @@ const EditModal = () => {
   };
 
   return (
-      <Modal
-      className='modal-container'
-        title="Rename your board"
-        visible={state.isEditBoardModalShown}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Input
-           defaultValue={state.currentBoard?.title} 
-           onChange={(event) => {
-            actions.setBoadrName(event.target.value)
-          }}
-        />
-      </Modal>
+    <Modal
+      className="modal-container"
+      title="Rename your board"
+      visible={state.isEditBoardModalShown}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <Input
+        defaultValue={state.currentBoard?.title}
+        onChange={(event) => {
+          actions.setBoadrName(event.target.value);
+        }}
+      />
+    </Modal>
   );
 };
 
